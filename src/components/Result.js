2664c8689b35d89c5bcd11209f274e82a02f6fc1
seojiu <seojiu5598@gmail.com>
2024-12-14 06:29:21 +0900

    feat: 결과 페이지 생성

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "../styles/DiagnosisForm.css";
 
 const Result = () => {
-  const uploadedImage = sessionStorage.getItem("uploadedImage");
-  const prediction = sessionStorage.getItem("prediction");
+  const [uploadedImage, setUploadedImage] = useState(null);
+  const [prediction, setPrediction] = useState("");
+
+  useEffect(() => {
+    // 세션 스토리지에서 데이터 가져오기
+    setUploadedImage(sessionStorage.getItem("uploadedImage"));
+    setPrediction(sessionStorage.getItem("prediction"));
+  }, []);
 
   return (
-    <div>
-      <h2>Diagnosis Result</h2>
-      <p>Prediction: {prediction}</p>
-      {uploadedImage && <img src={uploadedImage} alt="Uploaded" />}
+    <div className="container">
+      <h1>Diagnosis Result</h1>
+      {uploadedImage && <img src={uploadedImage} alt="Uploaded Preview" />}
+      <p>{prediction ? `AI Prediction: ${prediction}` : "No data available."}</p>
+      <button onClick={() => (window.location.href = "/")}>Back to Home</button>
     </div>
   );
 };
