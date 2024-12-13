6b562295772a02ce7cb5591c7716c1b65d0f9e7f
seojiu <seojiu5598@gmail.com>
2024-12-14 06:08:06 +0900

    feat: 시작페이지 추가

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import DiagnosisForm from "./components/DiagnosisForm";
 import Result from "./components/Result";
+import Home from "./components/Home";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<DiagnosisForm />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/diagnosis-form" element={<DiagnosisForm />} />
         <Route path="/result" element={<Result />} />
       </Routes>
     </Router>
